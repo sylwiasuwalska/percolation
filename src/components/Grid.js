3c65afb6de3060de3ba25bed73716c31b1bfab27
Sylwia Suwalska <sylwia.suwalska@gmail.com>
2020-03-20 18:28:20 +0100

    grid with colors

--- src/components/Grid.js
+++ src/components/Grid.js
@@ -2,22 +2,27 @@ import React from "react";
 import PropTypes from "prop-types";
 
 class Grid extends React.Component {
-	drawSquare = index => {
-		return <canvas className="square" key={index}></canvas>;
+	drawSquare = (index, val) => {
+		let color = "#6c5aa6";
+		val == 1 ? (color = "#6c5aa6") : (color = "#f36e62");
+		return (
+			<div
+				className="square"
+				style={{ backgroundColor: color }}
+				key={index}
+			>
+				{val}
+			</div>
+		);
 	};
 
-	drawGrid = () => {
-		//console.log(squares);
-
-		{
-		}
-	};
 	render() {
 		let squares = [...Array(100).keys()];
 		return (
 			<div className="container">
 				{squares.map(n => {
-					return this.drawSquare(n);
+					let val = Math.round(Math.random());
+					return this.drawSquare(n, val);
 				})}
 			</div>
 		);
